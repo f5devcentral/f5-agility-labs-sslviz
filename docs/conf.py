9e36cbf71cb7f9fcbf9ba2d20b067b1d6e402f5c
f5jason <f5jason@users.noreply.github.com>
2025-02-06 17:31:44 -0500

    AW2025 class 6 updates

--- docs/conf.py
+++ docs/conf.py
@@ -48,8 +48,8 @@ rst_prolog = """
 .. |ve|  replace:: Virtual Edition
 .. |icr| replace:: iControl REST API
 .. |ics| replace:: iControl SOAP API
-.. |f5|  replace:: F5 Networks
-.. |f5i| replace:: F5 Networks, Inc.
+.. |f5|  replace:: F5
+.. |f5i| replace:: F5, Inc.
 .. |year| replace:: %s
 .. |github_repo| replace:: %s
 """ % (classname,
@@ -171,8 +171,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = classname
-copyright = '2019-2024, F5 Networks, Inc.'
-author = 'F5 Networks, Inc.'
+copyright = '2019-2025, F5, Inc.'
+author = 'F5, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -284,7 +284,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, '%s.tex' % cleanname, '%s Documentation' % classname,
-     'F5 Networks, Inc.', 'manual', True),
+     'F5, Inc.', 'manual', True),
 ]
 
 # -- Options for manual page output ---------------------------------------
